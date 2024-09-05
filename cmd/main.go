2f00d74a4fa44bc02fad31974f262a1cbc4641c0
PNYwise <bunayya12345@gmail.com>
2024-09-05 11:41:39 +0700

    init

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/PNYwise/graha-bank-transaction-data-fixer-tool/internal"
+)
+
+func main()  {
+	
+	/**
+	 * Open DB connection
+	 *
+	**/
+	internal.ConnectDb()
+	defer func() {
+		if err := internal.CloseDb(); err != nil {
+			log.Fatalf("Error closing database connection: %v", err)
+		}
+	}()
+
+	if err := internal.Ping(); err != nil {
+		log.Fatalf("Error ping database connection: %v", err)}
+}
